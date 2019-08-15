e1f282a89664704cc6701922f6648e3ab5d62670
Vlad6555z <Vlad6555z@gmail.com>
2019-08-15 12:44:14 +0300

    create input component validation auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
 import clasess from "./Auth.module.css";
 import Button from "../../component/UI/Button/Button";
+import Input from "../../component/UI/Input/Input";
 
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formControls: {
+        email: {
+          value: "",
+          type: "email",
+          label: "Email",
+          errorMessage: "Введите коректный email",
+          valid: false,
+          touched: false,
+          validation: {
+            required: true,
+            email: true
+          }
+        },
+        password: {
+          value: "",
+          type: "password",
+          label: "Password",
+          errorMessage: "Введите коректный password",
+          valid: false,
+          touched: false,
+          validation: {
+            required: true,
+            minLength: 6
+          }
+        }
+      }
+    };
+  }
   loginHendler = () => {
     console.log("вы вошли");
   };
@@ -15,14 +47,37 @@ class Auth extends Component {
     event.preventDefault();
   };
 
+  onChangeHendler = (event, controlName) => {
+    console.log(`${controlName}: `, event.target.value);
+  };
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHendler(event, controlName)}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <div className={clasess.Auth}>
         <div>
           <h1 className={clasess.title}>Авторизация</h1>
           <form onSubmit={this.submitHendler} className={clasess.AuthForm}>
-            <input type="text" />
-            <input type="text" />
+            {this.renderInputs()}
+
             <Button type="success" onClick={this.loginHendler}>
               Войти
             </Button>
